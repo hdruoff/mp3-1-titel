73dc2870aa489bd821bdeedea920e3561f45db3c
ha-dida <hdruoff@gmx.de>
2023-05-22 20:59:43 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,27 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (items.length > 6) {
+    if (items.length > 0) {
         randomIndex = randint(0, items.length - 1)
         item = items[randomIndex]
-        items[randomIndex] = items[items.length] - 1
+        items[randomIndex] = items[items.length - 1]
         items.pop()
         basic.showNumber(item)
     } else {
         basic.showString("Done")
     }
 })
-function Disko () {
-    strip.rotate(1)
-    strip.show()
-    basic.pause(100)
-}
-let item: Arr = null
+let item = 0
 let randomIndex = 0
 let items: number[] = []
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB_RGB)
+// Liste mit Zahlen
+items = [
+1,
+2,
+3,
+4,
+5,
+6
+]
+let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB_RGB)
 strip.setBrightness(100)
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
@@ -30,14 +33,6 @@ strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
 strip.setPixelColor(7, neopixel.colors(NeoPixelColors.White))
 strip.show()
 let Anzahl_Klicks = 0
-items = [
-1,
-2,
-3,
-4,
-5,
-6
-]
 basic.forever(function () {
-    strip.clear()
+	
 })
