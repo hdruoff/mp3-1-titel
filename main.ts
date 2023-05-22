89702d86e230f9e7ee3cde166761229b289cdda2
ha-dida <hdruoff@gmx.de>
2023-05-22 21:54:59 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,31 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     } else {
         basic.showString("Done")
     }
+    if (item == 1) {
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else if (item == 2) {
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else if (item == 3) {
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else if (item == 4) {
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else if (item == 5) {
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else if (item == 6) {
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+    } else {
+        strip.clear()
+    }
 })
 let item = 0
 let randomIndex = 0
+let strip: neopixel.Strip = null
 let items: number[] = []
 // Liste mit Zahlen
 items = [
@@ -21,7 +43,7 @@ items = [
 5,
 6
 ]
-let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB_RGB)
+strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB_RGB)
 strip.setBrightness(100)
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
@@ -32,7 +54,9 @@ strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Green))
 strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
 strip.setPixelColor(7, neopixel.colors(NeoPixelColors.White))
 strip.show()
-let Anzahl_Klicks = 0
+basic.showString("Starten")
+basic.pause(1000)
+strip.clear()
 basic.forever(function () {
 	
 })
